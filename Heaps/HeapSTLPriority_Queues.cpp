604fe24b0c49a8e3ecd5ced39011e41845e8253d
Dilip <dilip45377@gmail.com>
2023-03-02 19:34:02 +0530

    added MinHeap

--- Heaps/HeapSTLPriority_Queues.cpp
+++ Heaps/HeapSTLPriority_Queues.cpp
@@ -24,12 +24,22 @@ int main(){
     minHeap.push(90);
     minHeap.push(10);
     minHeap.push(70);
+
+    cout<<"Printing Min Heap \n";
+    while(!minHeap.empty()){
+
+        cout<<minHeap.top()<<" ";
+
+        minHeap.pop();
+    }
     
     cout<<endl<<"Min Heap"<<endl;
     cout<<"Top most element -> "<<minHeap.top()<<endl;
     minHeap.pop();
     cout<<"After deleting Top most element -> "<<minHeap.top()<<endl;
     cout<<"The size of the priority Queue -> "<<minHeap.size()<<endl;
+
+
 
     return 0;
 }
